418fc27826c044c2053262d9f1db5add4702345f
SUMAN20140411 <nba20140411@gmail.com>
2025-08-12 09:50:34 +0900

    changgin the footer also index kor from gemini

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -286,26 +286,21 @@ export default function HomePage({ content }: HomePageProps) {
 
         <section className="relative z-30 bg-white px-4 md:px-6 ">
           <motion.div
-            className="text-left text-black mb-20 max-w-7xl mx-[30px] md:mx-[120px] text-base sm:text-lg lg:text-2xl font-semibold tracking-wide"
+            className="text-left text-black mb-10 max-w-7xl mx-[30px] md:mx-[120px] text-base sm:text-lg lg:text-2xl font-semibold tracking-wide"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.3 }}
             variants={fadeInVariants}
           >
             <p className={`${labelClass} mt-12`}>Service</p>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <h2 className="text-xl md:text-2xl lg:text-4xl font-bold tracking-wide text-black leading-normal -mt-20 lg:mt-16 mb-5">
-              고객 맞춤형 장비/제조 서비스를 제조합니다
+            <h2 className="text-xl md:text-2xl lg:text-4xl font-bold tracking-wide text-black leading-normal -mt-0 lg:mt-5 mb-5">
+              고객 맞춤형 장비/제조 서비스를 제공합니다
             </h2>
 
             <p className="text-sm md:text-sm lg:text-xl text-black/80 font-medium leading-relaxed mb-8">
               수만은 고객의 특정한 요구사항을 면밀히 분석하여 최적화된 맞춤형 장비 및 설비, <br />
               최고 품질의 정밀 가공 부품을 제공함으로써 혁신적인 솔루션과 지속적인 기술 지원을 통해<br />
-              제공함으로써 혁신적인 솔루션과 지속적인 기술 지원을 통해 고객 비즈니스의 성공을 이끌어갑니다.
+              고객 비즈니스의 성공을 이끌어갑니다.
             </p>
           </motion.div>
 
@@ -390,7 +385,7 @@ export default function HomePage({ content }: HomePageProps) {
             className="w-full object-cover"
             style={{ aspectRatio: "1440/220" }}
           />
-          <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-6 pointer-events-none transfrom lg:translate-x-20">
+          <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-6 pointer-events-none">
             <h2 className="text-sm md:text-xl lg:text-4xl font-semibold md:font-semibold lg:font-bold mb-2 md:mb-4 lg:mb-7 tracking-wide">
               Contact us
             </h2>
