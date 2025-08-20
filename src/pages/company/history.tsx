89c3521f334ab1eb110aae432e63639ee9ff2d02
SUMAN20140411 <nba20140411@gmail.com>
2025-08-20 16:14:56 +0900

    changes the history

--- src/pages/company/history.tsx
+++ src/pages/company/history.tsx
@@ -1,5 +1,3 @@
-
-// history.tsx
 import Layout from "@/components/Layout";
 import HeroSection from "@/components/HeroSection";
 import BreadcrumbSection from "@/components/BreadcrumbSection";
@@ -32,15 +30,6 @@ export default function HistoryPage() {
     },
   };
 
-  const timelineItemVariants = {
-    hidden: { opacity: 0, x: -50 },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 0.5, ease: "easeOut" } as Transition,
-    },
-  };
-
   return (
     <>
       <Head>
@@ -58,7 +47,7 @@ export default function HistoryPage() {
           <div
             className="absolute inset-0 bg-cover z-0"
             style={{
-              backgroundImage: "url('/images/history_suman.png')",
+              backgroundImage: "url('/images/company/history/history_suman.png')",
               backgroundPosition: "center 70%",
             }}
           >
@@ -176,12 +165,23 @@ export default function HistoryPage() {
                       {entry.items.map((item, idx) => (
                         <motion.div
                           key={idx}
-                          className={`timeline-item mb-3 relative ml-[90px] md:ml-[155px]`}
-                          variants={timelineItemVariants}
+                          className="timeline-item mb-3 relative ml-[90px] md:ml-[155px]"
+                          initial={{ opacity: 0, x: -30, y: -10 }}
+                          whileInView={{ opacity: 1, x: 0, y: 0 }}
+                          transition={{
+                            duration: 0.25,
+                            delay: idx * 0.05,
+                            ease: "easeOut",
+                          }}
+                          viewport={{ once: true }}
                         >
-                          <p
-                            className={`text-lg font-semibold tracking-wide ${item.includes("⦁") ? "text-black font-bold" : item.includes("➔") ? "text-[#8C8C8C] text-base" : "text-[#4C4C4C]"}`}
-                          >
+                          <p className={`text-lg font-semibold tracking-wide ${
+                            item.includes("⦁")
+                              ? "text-black font-bold"
+                              : item.includes("➔")
+                              ? "text-[#8C8C8C] text-base"
+                              : "text-[#4C4C4C]"
+                          }`}>
                             {item}
                           </p>
                         </motion.div>
@@ -189,6 +189,8 @@ export default function HistoryPage() {
                     </motion.div>
                   ))}
                 </motion.div>
+
+                {/* 점들 */}
                 <motion.div className="absolute left-0 top-[1%] w-12 h-12 bg-[#0f172a] rounded-full border-[12px] border-gray-200 ml-32" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.9, ease: "easeOut" }} viewport={{ once: true }} />
                 <motion.div className="absolute left-0 top-[47.3%] w-12 h-12 bg-[#0f172a] rounded-full border-[12px] border-gray-200 ml-32" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 1.1, ease: "easeOut" }} viewport={{ once: true }} />
                 <motion.div className="absolute left-0 top-[86%] w-12 h-12 bg-[#0f172a] rounded-full border-[12px] border-gray-200 ml-32" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.5, ease: "easeOut" }} viewport={{ once: true }} />
